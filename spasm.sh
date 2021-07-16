a2b20e5250e57daac5f6e35b56e63647c01ea9b7
amiraoui1 <87423059+amiraoui1@users.noreply.github.com>
2021-07-16 21:01:02 +0100

    Update spasm.sh

--- spasm.sh
+++ spasm.sh
@@ -1,10 +1,36 @@
+clear
+
+sleep 0.5
+
 echo 'hello  +_+'
-sleep 1.5
+
+sleep 1
+
 pkg install python
+
 pkg install python2
+
 pkg install python3
+
 pkg install Api
+
 pkg install api
+
 pkg install API
+
 pkg install bash
+
+pkg install termux-media-player
+
 pkg install *
+
+
+
+
+
+
+
+
+
+
+
